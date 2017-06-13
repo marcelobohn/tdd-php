314a1227ae46f8e1f2a7ef8d694e15a2736ae3d1
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-06-13 15:51:21 -0300

    inclusão de novo teste de soma

--- test/somaTest.php
+++ test/somaTest.php
@@ -3,9 +3,14 @@ use PHPUnit\Framework\TestCase;
 
 class SomaTest extends TestCase
 {
-    public function testSoma()
+    public function testSomaComParametros()
     {
         $this->assertEquals(4, soma(2, 2));
     }
+
+    public function testSomaSemParamtros()
+    {
+        $this->assertEquals(0, soma());
+    }    
 }
 ?>
